fbb13f91c9e4f36093790874a850bf4139fd5e99
Ramya Krishna <ramya.cheedella@tynybay.io>
2023-03-09 12:42:16 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+   <main>
+      <h1 className='title'>Fun facts about React</h1>
+      <ul className='facts'>
+        <li> Was first released in 2013</li>
+        <li> Was originally created by Jordan Walke</li>
+        <li> Has well over 100k starts on Github</li>
+        <li> Is maintained by Facebook</li>
+        <li> Powers thousands of enterprise apps,including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <img src="../images/download.png" alt="logo"/>
+        <h3> React Facts</h3>
+        <h4> React Course - Project 1</h4>
+    </nav>
+  )
+}
+
+export default NavBar
